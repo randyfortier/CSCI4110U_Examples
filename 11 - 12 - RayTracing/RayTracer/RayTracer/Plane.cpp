31eff2a3851ed26308e70671269cfbf6924ad5f6
randyfortier <randy.fortier@gmail.com>
2018-11-29 11:51:05 -0500

    Added base project for ray tracer

---
+++ 11 - 12 - RayTracing/RayTracer/RayTracer/Plane.cpp
@@ -0,0 +1,13 @@
+#include <cmath>
+#include <iostream>
+
+#include "Plane.h"
+#include "Util.h"
+
+float Plane::intersectionPoint(Ray ray) {
+	// TODO
+}
+
+Colour Plane::calculateShading(Vector3 lightPos, Colour lightColour, Ray ray, float t) {
+	// TODO
+}
---
+++ 11 - 12 - RayTracing/RayTracer/RayTracer/Primitive.cpp
@@ -0,0 +1,4 @@
+#include "Primitive.h"
+
+Primitive::Primitive() {
+}
